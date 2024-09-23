f894331f3e3c2575bae7ef07461cb0bdc5a19d1d
viogabriel19 <ivodegange@gmailcom>
2024-09-23 15:28:11 -0300

    leer datos desde json

--- back.js
+++ back.js
@@ -1,7 +1,13 @@
 import { onEvent, sendEvent, startServer } from "soquetic";
+import fs from "fs";
 
 const Hoteles = ["Sheraton", "Palace", "Hilton"];
 
 onEvent("dameDatos", () => {return Hoteles});
+
+onEvent("dameDatosArchivo", () => {
+    let datos = fs.readFileSync("data/datos.json", "utf8");
+    return JSON.parse(datos)
+});
 
 startServer();
--- front.js
+++ front.js
@@ -1,13 +1,34 @@
 const cont = document.querySelector(".container");
 
 function pedirDatos(){
-    fetchData("dameDatos", (datos) =>{
+    fetchData("dameDatosArchivo", (datos) =>{
         datos.forEach(dato => {
+
+            const card = document.createElement('div');
+            card.classList.add('hotel-card');
+        
             const tittle = document.createElement('h2');
-            tittle.innerHTML = `${dato}`;;
+            tittle.innerHTML = `Hotel ${dato.nombre}`;;
             tittle.classList.add('tittle');
+        
+            const rating = document.createElement('p');
+            rating.innerHTML = `Calificacion ${dato.calificacion}`;;
+            rating.classList.add('rating');
+        
+            const ubication = document.createElement('p');
+            ubication.innerHTML = `Ubicacion ${dato.ubicacion}`;;
+            ubication.classList.add('Ubi');
+        
+            const rooms = document.createElement('p');
+            rooms.innerHTML = `Habitaciones ${dato.habitaciones}`;;
+            rooms.classList.add('rooms');
             
-            cont.appendChild(tittle);
+            card.appendChild(tittle);
+            card.appendChild(rating);
+            card.appendChild(ubication);
+            card.appendChild(rooms);
+
+            cont.appendChild(card);
         });
     })
 }
