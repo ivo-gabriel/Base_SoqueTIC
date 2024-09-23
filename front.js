2d00f41087feea4769343eca21961daeb0be0592
viogabriel19 <ivodegange@gmailcom>
2024-09-23 10:06:47 -0300

    funciona base

--- front.js
+++ front.js
@@ -1,7 +1,13 @@
+const cont = document.querySelector(".container");
+
 function pedirDatos(){
     fetchData("dameDatos", (datos) =>{
         datos.forEach(dato => {
-            console.log(dato);
+            const tittle = document.createElement('h2');
+            tittle.innerHTML = `${dato}`;;
+            tittle.classList.add('tittle');
+            
+            cont.appendChild(tittle);
         });
     })
 }
